c02a1a222beefae1378602f405043f1dcf08d3f8
Patryk Obara <dreamer.tan@gmail.com>
2020-10-04 00:12:03 +0200

    WIP works

--- src/misc/fs_utils.cpp
+++ src/misc/fs_utils.cpp
@@ -23,6 +23,8 @@
 
 #include "fs_utils.h"
 
+#include <cassert>
+#include <glob.h>
 #include <string>
 #include <sys/stat.h>
 
@@ -39,7 +41,7 @@ static std::string translate_to_glob_pattern(const std::string &win_path)
 {
 	std::string glob_pattern;
 	glob_pattern.reserve(win_path.size() * 4);
-	char one_char_pattern[5] = "[aA]";
+	char one_char_pattern[5] = "[Aa]";
 	for (char c : win_path) {
 		if (isalpha(c)) {
 			one_char_pattern[1] = tolower(c);
@@ -58,6 +60,9 @@ static std::string translate_to_glob_pattern(const std::string &win_path)
 			glob_pattern.push_back('\\');
 			glob_pattern.push_back(c);
 			continue;
+		default:
+			glob_pattern.push_back(c);
+			continue;
 		}
 	}
 	return glob_pattern;
@@ -65,9 +70,30 @@ static std::string translate_to_glob_pattern(const std::string &win_path)
 
 std::string to_posix_path(const std::string &win_path)
 {
+	// TODO if exact path (except separator) exists, then just return it
 	const std::string pattern = translate_to_glob_pattern(win_path);
-	DEBUG_LOG_MSG(":: %s", pattern.c_str());
-	return "";
+	glob_t pglob;
+	const int err = glob(pattern.c_str(), GLOB_TILDE_CHECK, nullptr, &pglob);
+	if (err == GLOB_NOMATCH) {
+		DEBUG_LOG_MSG(":: NOMATCH");
+		globfree(&pglob);
+		return "";
+	}
+	if (err != 0) {
+		DEBUG_LOG_MSG(":: other error");
+		globfree(&pglob);
+		return "";
+	}
+	if (pglob.gl_pathc > 1) {
+		DEBUG_LOG_MSG("Warning: searching for path '%s' gives ambiguous results:",
+		              win_path.c_str());
+		for (size_t i = 0; i < pglob.gl_pathc; i++) {
+			DEBUG_LOG_MSG("'%s'", pglob.gl_pathv[i]);
+		}
+	}
+	std::string ret = pglob.gl_pathv[0];
+	globfree(&pglob);
+	return ret;
 }
 
 #endif
