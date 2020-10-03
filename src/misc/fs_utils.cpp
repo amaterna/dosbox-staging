e320731bf43650711a71fd0079135c14164f3e9c
Patryk Obara <dreamer.tan@gmail.com>
2020-10-03 21:57:00 +0200

    WIP

---
+++ src/misc/fs_utils.cpp
@@ -0,0 +1,40 @@
+/*
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ *  Copyright (C) 2019-2020  The dosbox-staging team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef DOSBOX_FS_UTILS_H
+#define DOSBOX_FS_UTILS_H
+
+#include "fs_utils.h"
+
+#include <string>
+#include <sys/stat.h>
+
+bool fs_exists(const char *path) noexcept
+{
+	struct stat sb;
+	return (stat(path, &sb) == 0);
+}
+
+std::string to_posix_path(const char *ipath)
+{
+	return "";
+}
+
+#endif
---
+++ tests/fs_utils.cpp
@@ -0,0 +1,52 @@
+/*
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ *  Copyright (C) 2020-2020  The dosbox-staging team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "fs_utils.h"
+
+#include <gtest/gtest.h>
+
+#include <string>
+
+namespace {
+
+TEST(PathExists, DirExists)
+{
+	EXPECT_TRUE(fs_exists("tests"));
+}
+
+TEST(PathExists, FileExists)
+{
+	EXPECT_TRUE(fs_exists("tests/fs_utils.cpp"));
+}
+
+TEST(PathExists, MissingPath)
+{
+	EXPECT_FALSE(fs_exists("foobar"));
+}
+
+TEST(PathConversion, SimpleTest_DISABLED)
+{
+	constexpr auto expected_result = "tests/files/paths/empty.txt";
+	constexpr auto input = "tests/files/PATHS/EMPTY.TXT";
+	ASSERT_TRUE(fs_exists(expected_result));
+	EXPECT_EQ(expected_result, to_posix_path(input));
+}
+
+} // namespace
